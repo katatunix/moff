206bb280c4e1bbc43a65493f3d45c0cdf24e5915
katatunix <katatunix@gmail.com>
2017-11-26 18:41:49 +0700

    rename folder release

--- release.sh
+++ release.sh
@@ -1,6 +1,13 @@
+rm -dfr release
+
+targetDir=release/moff
+
+mkdir -p $targetDir
 rm -dfr moff/build/tmp
-cp -Rpv moff/build .
-rm build/*.xml
-rm build/*.pdb
-rm build/System.ValueTuple.*
-cp -pv readme.md build
+cp -Rpv moff/build/* $targetDir
+
+rm $targetDir/*.xml
+rm $targetDir/*.pdb
+rm $targetDir/System.ValueTuple.*
+
+cp -pv readme.md $targetDir
